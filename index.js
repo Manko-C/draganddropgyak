88bc24467bc815a456caa84f2b5c00242a8d28ae
Manko-C <csalamartin2@gmail.com>
2024-12-13 13:12:11 +0100

    torleskell

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
 const betuk = document.getElementById("betuk");
 const jatekmezo = document.getElementById("jatekmezo");
-let letters = ['A','B','C','D','E','F','G','H','I',"J",'K','L','M','N','O','P','Q','R','S','T','U'];
+let letters = ['A','B','C','D','E','F','G','H','I',"J",'K','L','M','N','O','P','Q','R','S','T','U', 'V', "W", "X", "Y", "Z"];
+const elemek = document.getElementById("elemek");
 
 
-for (let i = 0; i<letters.length; i++){
-    const betu = document.createElement("div");
-    betu.innerText = letters[i];
-    betu.className = "betuim";
-    betu.draggable = true;
-    betu.style.width = '45px'
-    betu.style.height = '45px'
-    betuk.appendChild(betu);
-    };
+    for (let i = 0; i< 35; i++){
+        var betu = document.createElement("div");
+        let random = Math.floor(Math.random()* letters.length)
+        betu.innerText = letters[random];
+        betu.className = "betuim";
+        betu.draggable = true;
+        betu.style.width = '45px'
+        betu.style.height = '45px'
+        betuk.appendChild(betu);
 
-function palya(){
-    for (let i = 0; i< 10; i++){
-        var sor = document.createElement('div');
-        for (let j = 0; j< 10; j++){
-            var mezo = document.createElement("div");
-            mezo.className = "mezo";
-            sor.appendChild(mezo);
-            sor.className = "sor";
         };
-        jatekmezo.appendChild(sor);
+        function palya(){
+            for (let i = 0; i< 10; i++){
+                var sor = document.createElement('div');
+                for (let j = 0; j< 10; j++){
+                    var mezo = document.createElement("div");
+                    mezo.className = "mezo";
+                    sor.appendChild(mezo);
+                    sor.className = "sor";
+                };
+                jatekmezo.appendChild(sor);
+        
+            };
+            const huzhato = document.querySelectorAll('.betuim')
+            huzhato.forEach(huzhato => {
+                huzhato.addEventListener("dragstart", (event) =>{
+            
+                    event.dataTransfer.setData('text/plain', event.target.innerText);
+                })
+            })
+        
+            const mezoresz = document.querySelectorAll('.mezo')
+            mezoresz.forEach(mezo =>{
+                mezo.addEventListener("dragover", (event) =>{
+                    event.preventDefault();
+                });
+            });
+        
+            mezoresz.forEach(mezo => {
+                mezo.addEventListener("drop", (event) =>{
+                    event.preventDefault();
+                    const id = event.dataTransfer.getData('text/plain');
+                    const div = document.createElement("div");
+                    div.innerText = id;
+                    div.className = "betuim";
+                    div.draggable = true
+                    div.style.border = "none"
+                    mezo.appendChild(div);
+                });
+            });
+             
+        }
 
-    };
-    const huzhato = document.querySelectorAll('.betuim')
-    huzhato.forEach(huzhato => {
-        huzhato.addEventListener("dragstart", (event) =>{
-            event.dataTransfer.setData('text/plain', event.target.id);
-            event.target.style.backgroundColor = "aqua";
-        })
-    })
-    const mezoresz = document.querySelectorAll('.mezo')
-    mezoresz.forEach(mezo =>{
-        mezo.addEventListener("dragover", (event) =>{
-            event.preventDefault();
-            mezo.style.backgroundColor = "red";
-        });
-        /*mezo.addEventListener("dragleave", (event) =>{
-            event.preventDefault();
-            mezo.style.backgroundColor = "black";
-        });*/
-        mezo.addEventListener("drop", (event) =>{
-            event.preventDefault();
-            const id = event.dataTransfer.getData('text/plain');
-            const dragelement = document.getElementsByClassName('.betuim')
-            mezo.appendChild(dragelement);
-        })
-    });
-    
-    
-    
-}
+
+
+
 
 palya();
-
 
